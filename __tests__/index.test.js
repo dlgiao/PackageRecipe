7fcf05e187a655e31045350d5939d0ae1f5ff2ce
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2025-04-29 12:08:15 +0200

    Update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,10 +3,10 @@
  */
 // index.test.js
 describe("index.js", () => {
-    it("greet function returns Hello world!", () => {
-      // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
-      const greet = require("../src/index");
-  
-      expect(greet()).toBe("Hello world! DDC24");
-    });
+  it("greet function returns Hello world!", () => {
+    // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
+    const greet = require("../src/index");
+
+    expect(greet()).toBe("Hello world! DDC24");
   });
+});
