56b8edcf0fd3c87385a7107ce9d8426a9ebd4ef2
Dac Linh Giao Nguyen <dlg.nguyen@gmail.com>
2025-04-29 12:17:29 +0200

    Update js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,11 +2,10 @@
  * Unit tests for the index.js
  */
 // index.test.js
-describe("index.js", () => {
-  it("greet function returns Hello world!", () => {
-    // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
-    const greet = require("../src/index");
+describe('index.js', () => {
+  it('greet function returns Hello world!', () => {
+    const greet = require('../src/index');
 
-    expect(greet()).toBe("Hello world! DDC24");
+    expect(greet()).toBe('Hello world! DDC24');
   });
 });
